ecd69cd12718216232d26d0cdb9e15afc2547fb9
noordean <ebroyeem90@gmail.com>
2019-05-26 17:29:37 +0100

    initialize project

---
+++ lib/tictac.ex
@@ -0,0 +1,18 @@
+defmodule Tictac do
+  @moduledoc """
+  Documentation for Tictac.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tictac.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
